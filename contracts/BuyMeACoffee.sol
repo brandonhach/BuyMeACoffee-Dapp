777b9fc084b552ff59c8aee70da9e74ff2238133
Brandon Hach <58790036+bamboosmoothies@users.noreply.github.com>
2022-10-12 12:12:53 -0400

    commit

---
+++ contracts/BuyMeACoffee.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract BuyMeACoffee {
+    // Event to emit when a memo is created
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+
+    //Memo Struct.
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    //List of all memos received from friends.
+    Memo[] memos;
+
+    //Address of contract deployer.
+    address payable owner;
+
+    //Deploy Logic
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    /**
+     * @dev buy a coffee for contract owner
+     * @param _name name of the coffee buyer
+     * @param _message a nice message from the coffee buyer
+     */
+
+    function buyCoffee(string memory _name, string memory _message)
+        public
+        payable
+    {
+        //Check value
+        require(msg.value > 0, "can't buy coffee with 0 eth");
+
+        //Add memo to storage
+        memos.push(Memo(msg.sender, block.timestamp, _name, _message));
+
+        //Emit a log event when a new memo is created
+        emit NewMemo(msg.sender, block.timestamp, _name, _message);
+    }
+
+    /**
+     * @dev send the entire balance stored in this contract to the owner
+     */
+
+    function widthDrawTips() public {
+        //Strictly send to owner address no matter what
+        require(owner.send(address(this).balance));
+    }
+
+    /**
+     * @dev retrieve all the memos received and stored on the blockchain
+     */
+    function getMemos() public view returns (Memo[] memory) {
+        return memos;
+    }
+}
